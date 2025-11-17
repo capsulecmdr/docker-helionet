30c764a75efeef8725c99678c1d3083e3abe12fd
Orsiki <jwlittle13@gmail.com>
2025-11-17 06:24:01 -0700

    minor change on host_worker

--- host_worker.sh
+++ host_worker.sh
@@ -5,7 +5,7 @@ LOG_FILE="${1:-/tmp/host_worker.log}"
 
 echo "[$(date --iso-8601=seconds)] host_worker.sh starting..." >> "$LOG_FILE"
 
-while true; do
+
     # === Your actual work goes here ===
     echo "[$(date --iso-8601=seconds)] Running host worker task..." >> "$LOG_FILE"
     
@@ -13,4 +13,3 @@ while true; do
     # php /opt/seat-helionet/artisan some:command >> "$LOG_FILE" 2>&1
 
     #sleep 30
-done
